233a9e095d252cb45ecd703a81c81d48f60df35e
kartikey21 <kartikey.jain.21@outlook.com>
2020-12-03 15:50:22 +0530

    Second Commit// created Emp Model

---
+++ Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D49E02.Models
+{
+    public class Emp
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpDesignation { get; set; }
+        public DateTime EmpDOJ { get; set; }
+    }
+}
